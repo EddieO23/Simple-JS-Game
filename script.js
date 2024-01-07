e8a14ef6b5d814e0d915559202f4e50cf0fd9c88
EddieO23 <144298002+EddieO23@users.noreply.github.com>
2024-01-07 10:01:06 -0600

    added comments

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var character = document.getElementById("character"); 
 
 var block = document.getElementById("block");
-
+//function 
 function jump() {
     if (character.classList != "animate"){
         character.classList.add("animate");
@@ -11,7 +11,7 @@ function jump() {
         character.classList.remove("animate");
     }, 500);
 }
-
+// checking to see if pixels touch //
 var checkDead = setInterval(function(){
    var characterTop = parseInt (window.getComputedStyle(character).getPropertyValue("top"))
    var blockLeft = parseInt (window.getComputedStyle(block).getPropertyValue("left"))
